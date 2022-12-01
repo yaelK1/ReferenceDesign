b1713c8ba872cf1d598a28411fe49c3f178109c3
yaelK1 <108166521+yaelK1@users.noreply.github.com>
2022-12-01 17:41:37 +0200

    Update main.py

--- Podcastapp/main.py
+++ Podcastapp/main.py
@@ -153,9 +153,16 @@ def side_bar_and_tabs(option):
         placeholder_summary.markdown(f'<h1 style="font-size: 24px; color: #FFFFFF; font-weight: bold;">{summary_str}</h1>', unsafe_allow_html=True)
 
 def start_loader():
-    intro_str = "\n\n Press the Transcription tab to see the transcription  \n\n Press the sentiment tab to see the sentiments extracted from the video.\n Press the button next to the sentiment to see the corresponding part of the video. \n\n Press the chapters tab to see the chapters extracted from the video. \n Press the button next to the chapter to see the corresponding part of the video."
-    st.markdown(f'<h1 style="font-size: 24px; color: #FFFFFF; font-weight: bold;">Choose a file from the dropdown menu to get started.</h1>', unsafe_allow_html=True)
-    st.markdown(f'<h1 style="font-size: 24px; color: #FFFFFF; font-weight: bold;">{intro_str}</h1>', unsafe_allow_html=True) 
+    intro_str = """As an example, we used podcast episodes from YouTube and ran them through our text analytics pipeline """
+    instruction_str = """ Select an episode from the dropdown menu to see the results
+Press the Transcription tab to see the transcription.
+Press the sentiment tab to see the sentiments extracted from the video.
+Press the button next to the sentiment to see the corresponding part of the video. 
+Press the chapters tab to see the chapters extracted from the video. 
+Press the button next to the chapter to see the corresponding part of the video.
+"""
+    st.markdown(f'<p style="font-size: 24px; color: #FFFFFF; font-weight: bold;">{intro_str}</p>', unsafe_allow_html=True) 
+    st.markdown(f'<p style="font-size: 24px; color: #FFFFFF; font-weight: bold;">{instruction_str}</p>', unsafe_allow_html=True) 
     option = st.selectbox("",files_display())
     if not option is None:
         side_bar_and_tabs(option) 
